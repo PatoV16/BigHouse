52df462e71eeb723242971aa4e2168aa91be01b1
PatoV16 <133371436+PatoV16@users.noreply.github.com>
2025-01-09 10:49:52 -0500

    create backend

--- movil/casa_grande_app/lib/main.dart
+++ movil/casa_grande_app/lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'Pages/enfermeriaHomeScreen.dart';
 import 'Pages/LognForm.dart ';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:LoginScreen(),
+      home:NurseDashboard(),
     );
   }
 }
